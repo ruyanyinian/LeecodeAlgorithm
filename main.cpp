55cdf7598c392795b645d19829cac91a89a2afc4
ezxr.qinyu <qinyu@ezxr.com>
2021-09-15 17:53:18 +0800

    Revert "更新算法removeDuplicates"
    This reverts commit 8a7f073a777a6cf7eb64713ba6b177bc9bf4189c.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include "RemoveDuplicates.hpp"
+#include "1.TwoSum.hpp"
 
 int main()
 {
-    Solution remove;
-    vector<int> nums = {1, 1, 2, 2, 3};
-    int length = remove.removeDuplicates(nums);
-    cout << length << endl;
-    return 0;
+    Solution twoSumAns;
+    vector<int> numsInput = {2, 7, 11, 15};
+    vector<int> ans;
+    int target = 9;
+    ans = twoSumAns.twoSumHashMapOneIters(numsInput, 9);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << endl;
+    }
+    return 0; 
 }
