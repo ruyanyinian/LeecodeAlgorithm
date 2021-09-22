c2f30b9466ad638ef724ac9326d9fb7a75af6acb
qinyu <qinyufight123@126.com>
2021-09-22 22:03:13 +0800

    算法88的bug fixed，并且添加clean_project.sh脚本

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     Solution merged;
-    vector<int>nums1 = {1,2,3};
-    vector<int>nums2 = {2,3,4,5};
-    merged.merge(nums1, 3, nums2, 4);
+    vector<int>nums1 = {1,2,3,0,0,0};
+    vector<int>nums2 = {2,5,6};
+    merged.merge(nums1, 3, nums2, 3);
     for (int i=0; i<nums1.size(); i++)
     {
         cout << nums1[i] << endl;
